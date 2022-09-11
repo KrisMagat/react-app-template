2355ff481384bfc0ecbb40d5c81a8e6a5e241a2f
Kris Magat <kmag.dev@gmail.com>
2022-09-10 21:30:08 -0700

    upload files

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js',
+  },
+  devServer: {
+    port: 3010,
+    hot: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        sideEffects: true,
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
